54b4807d9f9dc559a70b0ad36f05ac5e9887865b
Hassan <hassan@panteragroup.org>
2017-03-17 15:47:14 +0500

    started configuring user registration form

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { HomeLibraryRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { NewUserComponent } from './new-user/new-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookListComponent,
+    NewUserComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    HomeLibraryRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
